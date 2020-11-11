ac0d16aeac77298089022cea0c1229f0ebf3044b
Dmitriy Moroz <vidjent@yandex.ru>
2020-11-11 20:17:48 +0300

    Add code

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,4 +4,15 @@
  */
 function camelize(str) {
   // ваш код...
+  let changeOne = str.split('-');
+
+  let changeTwo = changeOne.map(
+
+    (item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)
+    
+    );
+
+    let changeTree = changeTwo.join('');
+
+    return changeTree;
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+    let text = document.querySelector("#text");
+
+    function pos(){
+     (text.hidden != true) ? text.hidden = true : text.hidden = false;
+    }
+
+    button.addEventListener("click", pos );
 }
